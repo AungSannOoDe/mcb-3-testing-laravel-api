2e8d4ad95a541de053e681435fda9f8895230462
AungSannOoDe <theflash1321@gmail.com>
2026-01-22 19:56:10 +0630

    feat:add products  api

--- routes/api.php
+++ routes/api.php
@@ -8,3 +8,4 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 Route::get('/products/{id}', [ProductController::class, 'findByCategoryId']);
+Route::get('/products', [ProductController::class, 'index']);
